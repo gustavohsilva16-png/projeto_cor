8a07d2ebb4141feab6e96e56da076bfbd5d2de1d
GUSTAVO HENRIQUE SANTOS DA SILVA <26171882@edu119.local>
2026-09-17 14:07:56 -0300

    terminado

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
 const login = document.getElementById("txtLogin");
 const senha = document.getElementById("txtSenha");
-const DivResposta = getElementById("resposta");
-const botao = document.getElementById("resposta");
-function mostrar()
-{
-    const botao = document.getElementById("btnAcessar").addEventListener(
+const divResposta = document.getElementById("resposta");
+const botao = document.getElementById("btnAcessar"); // ID corrigido (btnAcessar)
 
-        DivResposta.innerHTML = login;
+const LoginBanco = "magolh@gmail.com";
+const senhaBanco = "123";
 
-    )
+function mostrar() {
+    const loginV = login.value;
+    const SenhaV = senha.value;
 
+    if (loginV === LoginBanco && SenhaV === senhaBanco) {
+        divResposta.textContent = "Login efetuado com sucesso";
+        setTimeout(() => {
+            window.location.href = "../paginas/home.html";
+        }, 2000);
+    } else {
+        divResposta.innerHTML = `<p>Login ou senha incorretos </p>`;
+    }
 }
+
+// O ouvinte de clique deve ser declarado no escopo global
+botao.addEventListener('click', mostrar);
